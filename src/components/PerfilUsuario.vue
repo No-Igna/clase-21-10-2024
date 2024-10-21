4e1b523e3716933ed0bebc78f9d082d880cd29df
josé Pérez <j.igna.perez@gmail.com>
2024-10-21 19:48:32 -0300

    activdades realizadas

--- src/components/PerfilUsuario.vue
+++ src/components/PerfilUsuario.vue
@@ -1,14 +1,32 @@
+<!-- Ejercicio 1: Creación de un componente de tarjeta de perfil con Bootstrap
+Crea un componente llamado PerfilUsuario.vue que muestre la información de un usuario, incluyendo su nombre, ocupación y una breve biografía. El diseño debe utilizar las tarjetas (card) de Bootstrap. Después, despliega el proyecto en GitHub Pages. -->
+
 <template>
+  <div class="container mt-3">
+    <div class="card col-4">
+      <div class="card-body">
+        <h5 class="card-title"><strong>Nombre:</strong> {{ perfil.nombre }}</h5>
+        <h6 class="card-subtitle mb-2 text-body-secondary"><strong>Ocupacion:</strong> {{ perfil.ocupacion }}</h6>
+        <p class="card-text">{{ perfil.biografia }}</p>
+      </div>
+    </div>
+  </div>
+  <hr>
 </template>
 
 <script>
 export default {
   name: 'PerfilUsuario',
   data() {
-    return {};
+    return {
+      perfil: {
+        nombre: "sujeto de pruebas",
+        ocupacion: "Ser el primer perfil creado",
+        biografia: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit minima sint ad tempore, magni voluptatum sapiente unde rem ipsa laboriosam in dignissimos a libero perspiciatis obcaecati corrupti dolore provident rerum?"
+      }
+    };
   }
 };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
